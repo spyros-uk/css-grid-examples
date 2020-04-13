4244e26c284f319bc452e452bb444f5a95d61cd8
Spyros <dev@spyros.uk>
2020-04-13 14:00:00 +0100

    Adds frid-template shorthand example

--- src/grid-template-shorthand/index.js
+++ src/grid-template-shorthand/index.js
@@ -5,11 +5,5 @@ document.querySelector("#app").innerHTML = `
   <div class="grid__item">2</div>
   <div class="grid__item">3</div>
   <div class="grid__item">4</div>
-  <div class="grid__item">5</div>
-  <div class="grid__item">6</div>
-  <div class="grid__item">7</div>
-  <div class="grid__item">8</div>
-  <div class="grid__item">9</div>
-  <div class="grid__item">10</div>
 </div>
 `
