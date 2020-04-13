709133963adbd58c7f7e5474c80a51a49b8e697d
Spyros <dev@spyros.uk>
2020-04-13 13:33:38 +0100

    Adds sub grid example

---
+++ src/sub-grid/index.js
@@ -0,0 +1,22 @@
+import "./styles.scss"
+document.querySelector("#app").innerHTML = `
+<div class="grid grid--level-1">
+  <div class="grid__item">1</div>
+  <div class="grid__item">2</div>
+  <div class="grid__item">3</div>
+  <div class="grid__item">4</div>
+  <div class="grid grid--level-2">
+    <div class="grid__item">a</div>
+    <div class="grid__item">b</div>
+    <div class="grid__item">c</div>
+    <div class="grid__item">d</div>
+    <div class="grid__item">e</div>
+    <div class="grid__item">f</div>
+  </div>
+  <div class="grid__item">6</div>
+  <div class="grid__item">7</div>
+  <div class="grid__item">8</div>
+  <div class="grid__item">9</div>
+  <div class="grid__item">10</div>
+</div>
+`
