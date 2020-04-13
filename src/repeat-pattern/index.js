0878ac3e2967ed74042837d48977e7547a131a70
Spyros <dev@spyros.uk>
2020-04-13 12:18:33 +0100

    Adds repeat pattern example

---
+++ src/repeat-pattern/index.js
@@ -0,0 +1,15 @@
+import "./styles.scss"
+document.querySelector("#app").innerHTML = `
+<div class="grid">
+  <div class="grid__item">1</div>
+  <div class="grid__item">2</div>
+  <div class="grid__item">3</div>
+  <div class="grid__item">4</div>
+  <div class="grid__item">5</div>
+  <div class="grid__item">6</div>
+  <div class="grid__item">7</div>
+  <div class="grid__item">8</div>
+  <div class="grid__item">9</div>
+  <div class="grid__item">10</div>
+</div>
+`
