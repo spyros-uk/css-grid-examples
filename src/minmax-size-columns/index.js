9609a74a26eba2e685e335f72e61d528c62784ae
Spyros <dev@spyros.uk>
2020-04-13 12:02:26 +0100

    Adds minmax sized grid example

---
+++ src/minmax-size-columns/index.js
@@ -0,0 +1,8 @@
+import "./styles.scss"
+document.querySelector("#app").innerHTML = `
+<div class="grid">
+  <div class="grid__item">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+  <div class="grid__item">2</div>
+  <div class="grid__item">3</div>
+</div>
+`
